a326d6460f3e86f8f6038ea30584b0747c02e1aa
Valodya <10vova.demiraki@gmail.com>
2022-02-13 18:54:48 +0200

    commit about v1.1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,8 +9,23 @@ import {AuthService} from "../shared/services/auth-service";
 export class AboutComponent implements OnInit {
 
   constructor(public authService: AuthService,) { }
+  images =[
+    "assets/img/Nachalnik.png",
+    "assets/img/Valodya.png",
+    "assets/img/Zayolio.png"
+  ];
+  curImageIndex=0;
+  curImage!: string;
 
   ngOnInit(): void {
+    this.curImage = this.images[this.curImageIndex];
+    setInterval(()=>{
+      this.curImageIndex++;
+      if(this.curImageIndex >= this.images.length){
+        this.curImageIndex=0;
+      }
+      this.curImage = this.images[this.curImageIndex];
+    },10000)
   }
 
 }
